c6a4815c68278921d84855b4217873aabe1ec50c
Deeksha Koul <deekshakoul.daiict@gmail.com>
2025-04-06 22:26:07 +0530

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -1,30 +1,41 @@
 import numpy as np
 import os
+import torch
 from rank_bm25 import BM25Okapi
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+MODEL_NAME = 'sentence-transformers/all-mpnet-base-v2'
 
 class BM25Retriever:
     """
     Sparse retrieval using BM25 algorithm
     """
-    def __init__(self):
+    def __init__(self, passages):
         self.tokenized_corpus = None
         self.bm25 = None
-        self.passages = None
-    
-    def index(self, passages):
-        """
-        Index the corpus of passages
-        """
         self.passages = passages
+        if os.path.exists("data/bm25_index.pkl"):
+            with open("data/bm25_index.pkl", "rb") as f:
+                self.bm25 = pickle.load(f)
+        else:
+            self.bm25 = None
+
+
+    def index(self, save=False):
+        if save and os.path.exists("data/bm25_index.pkl"):
+            return self.bm25
         tokenized_corpus = []
-        for passage in passages:
+        for passage in self.passages:
             tokens = passage.split()
             tokenized_corpus.append(tokens)
         
         self.tokenized_corpus = tokenized_corpus
         self.bm25 = BM25Okapi(tokenized_corpus)
+
+        if save:
+            with open("data/bm25_index.pkl", "wb") as f:
+                pickle.dump(self.bm25, f)
     
     def retrieve(self, query, k=10):
         """
@@ -38,21 +49,30 @@ class BM25Retriever:
         return top_passages, top_scores, top_indices
 
 
+        
+
+
+
 class DenseRetriever:
     """
     Dense retrieval using pre-trained sentence transformers
     """
-    def __init__(self, model_name="all-MiniLM-L6-v2"):
+    def __init__(self, passages, model_name=MODEL_NAME):
         self.model = SentenceTransformer(model_name)
-        self.passage_embeddings = None
-        self.passages = None
-    
-    def index(self, passages):
-        """
-        Index the corpus of passages by computing embeddings
-        """
         self.passages = passages
-        self.passage_embeddings = self.model.encode(passages, convert_to_tensor=True)
+        if os.path.exists("data/DPR_index.pt"):
+            self.passage_embeddings = torch.load("data/DPR_index.pt")
+        else:
+            self.passage_embeddings = None
+
+    
+    def index(self, save=False):
+        if save and os.path.exists("data/DPR_index.pt"):
+            return self.passage_embeddings
+        self.passage_embeddings = self.model.encode(self.passages, convert_to_tensor=True)
+
+        if save:
+            torch.save(self.passage_embeddings, "data/DPR_index.pt")
     
     def retrieve(self, query, k=10):
         """
